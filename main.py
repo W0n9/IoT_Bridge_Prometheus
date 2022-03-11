057892b16688ace4dc179b1864e2122f7bfb1eba
W0n9 <38560218+W0n9@users.noreply.github.com>
2022-03-11 13:32:30 +0800

    use logging module; change label

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import logging
 import threading
 import time
 
@@ -6,21 +7,23 @@ from prometheus_client import Gauge, start_http_server
 
 from read_sensor import read_sensor
 
+logging.basicConfig(format="%(asctime)s %(message)s", level=logging.DEBUG)
+
 # Read config file
 with open("config.yaml", "r", encoding="utf-8") as f:
     config = yaml.load(f, Loader=yaml.FullLoader)
-    print(config)
+    logging.info(config)
 
 # Create a metric to track temperature and humidity
 gt = Gauge(
     "temperature_metric_celsius",
     "Temperature measured by the WRD Sensor",
-    ["instance", "campus", "building", "room"],
+    ["node", "campus", "building", "room"],
 )
 gh = Gauge(
     "humidity_metric_ratio",
     "Humidity percentage measured by the WRD Sensor",
-    ["instance", "campus", "building", "room"],
+    ["node", "campus", "building", "room"],
 )
 
 
@@ -29,33 +32,33 @@ def write_prometheus(sensor):
         try:
             temp, hum, _ = read_sensor(sensor["ip"], 80)
         except:
-            print(sensor["ip"] + " Error")
-            print(_)
+            logging.error(sensor["ip"] + " Error")
             continue
         # print("Temperature: %s, Humidity: %s" % (temp, hum))
-        try:
-            gt.labels(
-                instance=sensor["ip"],
-                campus=sensor["campus"],
-                building=sensor["building"],
-                room=sensor["room"],
-            ).set(temp)
-        except:
-            print(sensor["ip"] + "Temp Error")
-            print(_)
-            continue
-        try:
-            gh.labels(
-                instance=sensor["ip"],
-                campus=sensor["campus"],
-                building=sensor["building"],
-                room=sensor["room"],
-            ).set(hum)
-        except:
-            print(sensor["ip"] + "Humd Error")
-            print(_)
-            continue
-        time.sleep(1)
+        if _ != None:
+            try:
+                gt.labels(
+                    node=sensor["ip"],
+                    campus=sensor["campus"],
+                    building=sensor["building"],
+                    room=sensor["room"],
+                ).set(temp)
+            except:
+                logging.error(sensor["ip"] + " Temp Error")
+                print(_)
+                continue
+            try:
+                gh.labels(
+                    node=sensor["ip"],
+                    campus=sensor["campus"],
+                    building=sensor["building"],
+                    room=sensor["room"],
+                ).set(hum)
+            except:
+                logging.error(sensor["ip"] + " Humd Error")
+                print(_)
+                continue
+            time.sleep(1)
 
 
 if __name__ == "__main__":
