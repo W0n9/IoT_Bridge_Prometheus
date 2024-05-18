2014a14e174a5f48f86cdce6d6092bed687b4662
TsungWing <38560218+W0n9@users.noreply.github.com>
2024-05-18 23:31:22 +0800

    重构：使用异步编程模型

--- main.py
+++ main.py
@@ -1,18 +1,15 @@
+import asyncio
 import logging
-import threading
-import time
+from contextlib import asynccontextmanager
 
-import yaml
-from prometheus_client import Gauge, start_http_server
+from fastapi import FastAPI
+from prometheus_client import Gauge, make_asgi_app
 
-from read_sensor import read_sensor
+from .config import Sensor, Settings, settings
+from .read_sensor import read_sensor
 
-logging.basicConfig(format="%(asctime)s %(message)s", level=logging.DEBUG)
+logging.basicConfig(format="%(asctime)s %(message)s", level=logging.INFO)
 
-# Read config file
-with open("config.yaml", "r", encoding="utf-8") as f:
-    config = yaml.safe_load(f)
-    logging.info(config)
 
 # Create a metric to track temperature and humidity
 gt = Gauge(
@@ -27,10 +24,10 @@ gh = Gauge(
 )
 
 
-def write_prometheus(sensor):
+async def write_prometheus(sensor: Sensor):
     while True:
         try:
-            temp, hum, _ = read_sensor(sensor["ip"], 80)
+            temp, hum, _ = await read_sensor(sensor.ip, 80)
             if hum == 0:
                 raise ValueError("Humidity is 0")
             if _ == None:
@@ -38,78 +35,103 @@ def write_prometheus(sensor):
             # print(temp, hum, _)
         except Exception as e:
             logging.error(
-                sensor["ip"] + sensor["campus"] + sensor["building"] + sensor["room"]
+                f"{sensor.ip} {sensor.campus} {sensor.building} {sensor.room}"
             )
-            logging.exception(e)
+            # logging.exception(e)
             try:
                 if isinstance(
                     gt.labels(
-                        sensor["ip"],
-                        sensor["campus"],
-                        sensor["building"],
-                        sensor["room"],
+                        sensor.ip,
+                        sensor.campus,
+                        sensor.building,
+                        sensor.room,
                     ),
                     Gauge,
                 ):
                     gt.remove(
-                        sensor["ip"],
-                        sensor["campus"],
-                        sensor["building"],
-                        sensor["room"],
+                        sensor.ip,
+                        sensor.campus,
+                        sensor.building,
+                        sensor.room,
                     )
             except Exception as e:
                 logging.exception(e)
             try:
                 if isinstance(
                     gh.labels(
-                        sensor["ip"],
-                        sensor["campus"],
-                        sensor["building"],
-                        sensor["room"],
+                        sensor.ip,
+                        sensor.campus,
+                        sensor.building,
+                        sensor.room,
                     ),
                     Gauge,
                 ):
                     gh.remove(
-                        sensor["ip"],
-                        sensor["campus"],
-                        sensor["building"],
-                        sensor["room"],
+                        sensor.ip,
+                        sensor.campus,
+                        sensor.building,
+                        sensor.room,
                     )
             except Exception as e:
                 logging.exception(e)
-            time.sleep(5)
+            await asyncio.sleep(5)
             continue
         # print("Temperature: %s, Humidity: %s" % (temp, hum))
         if _ != None:
             try:
                 gt.labels(
-                    node=sensor["ip"],
-                    campus=sensor["campus"],
-                    building=sensor["building"],
-                    room=sensor["room"],
+                    node=sensor.ip,
+                    campus=sensor.campus,
+                    building=sensor.building,
+                    room=sensor.room,
                 ).set(temp)
             except:
-                logging.error(sensor["ip"] + " Temp Error")
+                logging.error(f"{sensor.ip} Temp Error")
                 print(_)
                 continue
             try:
                 gh.labels(
-                    node=sensor["ip"],
-                    campus=sensor["campus"],
-                    building=sensor["building"],
-                    room=sensor["room"],
+                    node=sensor.ip,
+                    campus=sensor.campus,
+                    building=sensor.building,
+                    room=sensor.room,
                 ).set(hum)
             except:
-                logging.error(sensor["ip"] + " Humd Error")
+                logging.error(f"{sensor.ip} Humd Error")
                 print(_)
                 continue
-            time.sleep(1)
+            await asyncio.sleep(1)
 
+
+async def main(settings: Settings):
+    """
+    后台任务，用于持续读取传感器数据，写入prometheus
+    """
+    import asyncer
+
+    async with asyncer.create_task_group() as task_group:
+        [task_group.soonify(write_prometheus)(sensor) for sensor in settings.sensors]
+
+
+@asynccontextmanager
+async def background_task(app: FastAPI):
+    """
+    启动后台任务，再启动fastapi
+    https://fastapi.tiangolo.com/advanced/events/#lifespan
+    """
+    print(settings.sensors)
+    asyncio.create_task(main(settings))
+    yield
+
+
+# Create app
+app = FastAPI(debug=False, lifespan=background_task)
+
+# Add prometheus asgi middleware to route /metrics requests
+metrics_app = make_asgi_app()
+app.mount("/metrics", metrics_app)
 
 if __name__ == "__main__":
-    # Start up the server to expose the metrics.
-    start_http_server(9580, addr="0.0.0.0")
-    # 遍历传感器
-    for sensor in config["sensors"]:
-        t = threading.Thread(target=write_prometheus, args=(sensor,))
-        t.start()
+    import uvicorn
+
+    uvicorn.run(app, host="0.0.0.0", port=9580)
